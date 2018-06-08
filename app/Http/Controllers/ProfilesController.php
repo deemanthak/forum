2f932730439847981d580480c3c246beee614f9c
Deemantha Kasun <deemantha.thilakarathne@globaltote.com>
2018-06-08 08:28:36 +0530

    lesson 26

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -46,6 +46,9 @@ class ProfilesController extends Controller
      */
     public function show(User $user)
     {
+        $activities = $user->activity()->with('subject')->get()->groupBy(function($activity){
+            return $activity->created_at->format('Y-m-d');
+        });
 
         return view('profiles.show',[
             'profileUser'=>$user,
